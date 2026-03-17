b4a956b7fa8f8c5a14c626abd3c469dd954fb6a1
CharlesOliveira <oliveiracharles547@gmail.com>
2026-03-17 16:47:46 -0300

    configurando a API

--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Funcionario, ColetaFaces, Treinamento
 
-# Register your models here.
+
+class ColetaFacesInline(admin.StackedInline):
+    model = ColetaFaces
+    extra = 0
+
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+    inlines = (ColetaFacesInline,)
+
+
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Treinamento)
